96aa1165340c6a571c49b3f5775f9bdf605bcdf6
Soroush Mehraban <Smehraban2013@gmail.com>
2022-02-16 21:19:12 +0330

    Commit after watching the second video

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Soroush Mehraban',
+        'title': 'Blog post 1',
+        'content': "First post content",
+        "date_posted": "February 16, 2022"
+    },
+{
+        'author': 'John Doe',
+        'title': 'Blog post 2',
+        'content': "Second post content",
+        "date_posted": "February 16, 2022"
+    },
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello, World!</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return render_template('about.html', title='About')
